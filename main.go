7bd50523a90e716e2d8c64f1972a1809554c3b97
Vishnu Bharathi <vishnu@hasura.io>
2023-01-22 05:17:37 +0530

    app: basic prototype

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/buildkite/go-buildkite/v3/buildkite"
 	"github.com/gin-gonic/gin"
 )
 
@@ -34,9 +35,55 @@ func buildkiteServiceDiscoveryHandler(buildkiteToken string) func(c *gin.Context
 	// so test that case also
 	return func(c *gin.Context) {
 		log.Println("start of root request")
-		c.JSON(http.StatusOK, gin.H{
-			"message": "root",
-		})
+
+		config, err := buildkite.NewTokenConfig(buildkiteToken, false)
+		if err != nil {
+			c.Error(err)
+		}
+		buildkiteClient := buildkite.NewClient(config.Client())
+		var allAgents []buildkite.Agent
+
+		for currentPage, lastPage := 1, 0; (currentPage == 1) || (currentPage <= lastPage); {
+			// TODO: avoid hardcoding the org
+			agents, resp, err := buildkiteClient.Agents.List("hasura", &buildkite.AgentListOptions{
+				ListOptions: buildkite.ListOptions{
+					Page:    currentPage,
+					PerPage: 4,
+				},
+			})
+			if err != nil {
+				c.Error(err)
+			}
+			lastPage = resp.LastPage
+			currentPage++
+			allAgents = append(allAgents, agents...)
+		}
+
+		type PromSDEntry struct {
+			Targets []string          `json:"targets"`
+			Labels  map[string]string `json:"labels"`
+		}
+		var sdEntries []PromSDEntry
+
+		for _, agent := range allAgents {
+			entry := PromSDEntry{
+				Targets: []string{
+					*agent.IPAddress,
+				},
+				Labels: map[string]string{
+					"__meta__host_name":       *agent.Hostname,
+					"__meta__connected_state": *agent.ConnectedState, // TODO: see if you could filter by connected state in buildkite api itself
+				},
+			}
+
+			// TODO: agent.Metadata is an array of string. It could contain two queues with different vaules
+			// at that point, the agent is supposed to be serving both of the queues at the same time
+			// so have separate SD entries for them
+			sdEntries = append(sdEntries, entry)
+		}
+
+		c.JSON(http.StatusOK, sdEntries)
+
 		log.Println("end of root request")
 	}
 }
